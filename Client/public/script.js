b54277f720936cacf9b31449dac1579f0a2c0b5a
Augusto Xavier <augusto.xavier@outlook.com>
2021-10-25 19:40:25 -0300

    Add input required alert

--- Client/public/script.js
+++ Client/public/script.js
@@ -1,8 +1,15 @@
+const nameInput = document.getElementById("nameInput");
 const startButton = document.getElementById("startButton");
-var    startAnimation = new TimelineMax({repeat:0});
+var startAnimation = new TimelineMax({ repeat: 0 });
 
-startButton.onclick = function(){
-    console.log('Conecta com server e começa o jogo')
-    startAnimation.to([startButton, title, nameInput], 1, {alpha:0});
-}
+startButton.onclick = function () {
 
+  console.log(nameInput.value)
+  if (nameInput.value !== '') {
+    console.log("Conecta com server e começa o jogo");
+    startAnimation.to([startButton, title, nameInput], 1, { alpha: 0 });
+    startAnimation.to([startButton, title, nameInput], 0.1, {css: { display: "none" }});
+  } else {
+    alert("Digite um nome!")
+  }
+};
