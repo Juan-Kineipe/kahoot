525f7aebe3cf4d25fef029c68b5016e617a7b84a
Augusto Xavier <augusto.xavier@outlook.com>
2021-10-25 19:13:43 -0300

    Add animation

--- Client/public/script.js
+++ Client/public/script.js
@@ -1,6 +1,8 @@
 const startButton = document.getElementById("startButton");
+var    startAnimation = new TimelineMax({repeat:0});
 
 startButton.onclick = function(){
     console.log('Conecta com server e começa o jogo')
+    startAnimation.to([startButton, title, nameInput], 1, {alpha:0});
 }
 
