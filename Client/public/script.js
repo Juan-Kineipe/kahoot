6bf0b4b116e8b93274d86a07edb43b49d5be14de
Juan Kineipe <juan.kineipe@gmail.com>
2021-10-30 12:16:35 -0300

    fix: animation

--- Client/public/script.js
+++ Client/public/script.js
@@ -7,8 +7,8 @@ startButton.onclick = function () {
   console.log(nameInput.value)
   if (nameInput.value !== '') {
     console.log("Conecta com server e começa o jogo");
-    startAnimation.to([startButton, title, nameInput], 1, { alpha: 0 });
-    startAnimation.to([startButton, title, nameInput], 0.1, {css: { display: "none" }});
+    startAnimation.to([title, card], 1, { alpha: 0 });
+    startAnimation.to([title, card], 0.1, {css: { display: "none" }});
   } else {
     alert("Digite um nome!")
   }
